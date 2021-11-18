195b4a3c1b241642c7e5ab69b601971620249f4b
Rigers Leka <rigers.leka@sisal.al>
2021-11-18 09:41:44 +0100

    implement copyClipboard button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+  const [colorPicker, setColorPicker] = useState(props.baseColorState);
+  
+  const copyClipboard = () => {
+    navigator.clipboard.writeText(
+      `rgb (${colorPicker.r}, ${colorPicker.g}, ${colorPicker.b})`
+    );
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <button onClick={copyClipboard}>Copy clipboard</button>
     </div>
   );
 }
