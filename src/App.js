2a78714aa478cebae39be6bca3abdd22427e7208
Rigers Leka <rigers.leka@sisal.al>
2021-11-18 09:47:33 +0100

    implement swatchesBottomPicker component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
 
+import SwatchesBottomPicker from './SwatchesBottomPicker';
+
 function App(props) {
   const [colorPicker, setColorPicker] = useState(props.baseColorState);
-  
+
+  const handleColorChange = (event) => {
+    setColorPicker(event.rgb);
+  };
+
   const copyClipboard = () => {
     navigator.clipboard.writeText(
       `rgb (${colorPicker.r}, ${colorPicker.g}, ${colorPicker.b})`
     );
   };
 
-
   return (
-    <div className="App">
+    <div className='App'>
+      <div className='row'>
+        <SwatchesBottomPicker
+          color={colorPicker}
+          onChange={handleColorChange}
+        />
+      </div>
       <button onClick={copyClipboard}>Copy clipboard</button>
     </div>
   );
